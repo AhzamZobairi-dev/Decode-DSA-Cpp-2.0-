481d6bb1e68becb299a686b3fdefa6814d031f9a
Ahzam <ahzamzobairi402@gmail.com>
2023-12-17 03:51:46 +0530

    Added some notesabout use of Logical && | Patch 1

--- Assignment/Wk2/Condition2/01.Count_Min_Notes.cpp
+++ Assignment/Wk2/Condition2/01.Count_Min_Notes.cpp
@@ -4,6 +4,8 @@ Input 1: 510
 Output1 : notes of “500” = 1 and notes of “10” = 1
 */
 
+// NOTE -- THIS CODE CAN BE DONE ALSO USING LOOP SINCE TILL HERE TOPIC IS NOT COVERED,SO I HAVE ADDED
+// THIS CODE USING LOOP IN SAME REPOSITORY WITH NAME 99.Alternative_Sol_Count_Min_notes.cpp   
 
 #include <iostream>
 using namespace std;
@@ -57,6 +59,24 @@ int main()
             note_10 = amount / 10;
             amount = amount - (note_10 * 10);
     }
+
+/*  
+After pondering about the expression to put in switch condition I seems illogical to put the condition with extra expression with && here 
+
+for ex: - in switch condition of 200 Rs note
+    -- note_200 will fetch total number of 200 Rs note, even if amount is more than 200 and as well multiple of 200 like 400.
+    -- if the amount will exceed 400 in multiple of 200 i.e 600 then it will be taken by condition mention in first switch statement 
+                    i.e switch(amount >= 500)
+       no need to put any further condiiton as -- switch(amount >= 200 && amount < 500)
+                    switch ( amount >= 200 ) is enough
+
+-- Also not using any break statement because all cases are in different switch statement, Ideally break statement should be present in case code block 
+
+Moreover it is Ligical AND operation which will evalutate both side if the first expression is correct. But we do not need the condition
+after && as it is not logical. This wastes precious computing resources (compute power or memory).
+
+********----- I AM NOT CHANGING THE ABOVE CODE IN THIS PATCH TO DOCUMENT THE MISTAKES I HAVE MADE AND ALSO GOT SOME HELP FROM SOLUTIONS -------********           
+*/
 
     cout << "Notes of";
     if(note_500 > 0)   cout << " \"500\" = " << note_500;
