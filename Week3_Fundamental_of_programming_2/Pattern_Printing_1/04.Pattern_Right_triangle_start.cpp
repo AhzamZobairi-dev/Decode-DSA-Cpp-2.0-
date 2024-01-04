307b8cb2f841ba08489192d57ca5a811f9599717
Ahzam <ahzamzobairi402@gmail.com>
2024-01-04 07:57:26 +0530

    Star Triangle

---
+++ Week3_Fundamental_of_programming_2/Pattern_Printing_1/04.Pattern_Right_triangle_start.cpp
@@ -0,0 +1,28 @@
+/*      STAR PATTERN
+
+    *
+    **
+    ***
+    ****
+    *****
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout << "Enter the height of triangle : ";
+    cin >> row;
+    
+    for (int i = 1; i<=row;i++)
+    {
+        for(int j = 1;j<=i; j++)
+        {
+            cout << "* ";
+        }
+        cout <<endl;
+    }
+    return 0;
+}
