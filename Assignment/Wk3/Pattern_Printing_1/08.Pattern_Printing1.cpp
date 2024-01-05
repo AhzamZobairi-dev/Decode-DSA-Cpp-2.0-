56da7979583bffe62ebda0374d0fcbc6147116ab
Ahzam <ahzamzobairi402@gmail.com>
2024-01-05 17:58:14 +0530

    Assignment | Week 3 | Pattern 1
    Sample Input : n= 4
    Output :
    1
    1 2
    1 2 3
    1 2 3 4

---
+++ Assignment/Wk3/Pattern_Printing_1/08.Pattern_Printing1.cpp
@@ -0,0 +1,33 @@
+/*  Print the following pattern
+
+Sample Input : n= 4
+Output :
+1
+1 2
+1 2 3
+1 2 3 4
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int side;
+    cout << "Enter side of sqaure : ";
+    cin >> side;
+    
+
+    for(int i=1; i<=side; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    
+    }
+    
+    return 0;
+}
