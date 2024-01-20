acc952eadb4aadbc69e9a507f4af92aa2b4751c0
Ahzam <ahzamzobairi402@gmail.com>
2024-01-20 17:30:59 +0530

    Week3 | Pattern Pritning 1 | Added changes in Pattern_Reverse_star_triangle

--- Week3_Fundamental_of_programming_2/Pattern_Printing_1/05.Patterm_Reverse_star_triangle.cpp
+++ Week3_Fundamental_of_programming_2/Pattern_Printing_1/05.Patterm_Reverse_star_triangle.cpp
@@ -21,7 +21,7 @@ int main()
     cout <<"METHOD 1" << endl;
     for(int i=1; i<=row;i++)
     {
-        for (int j =4; j>=i; j--)
+        for (int j =row; j>=i; j--)
         {
             cout << "* ";
         }
@@ -33,6 +33,17 @@ int main()
     for(int i =1 ; i <= row; i++)
     {
         for(int j = 1; j <= row+1-i ; j++)      // if we use j<=i we will print star Traiangle
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+//METHOD 3
+    cout << endl << "METHOD 3" << endl;
+    for(int i =1 ; i <= row; i++)
+    {
+        for(int j = i; j <= row ; j++)      
         {
             cout << "* ";
         }
