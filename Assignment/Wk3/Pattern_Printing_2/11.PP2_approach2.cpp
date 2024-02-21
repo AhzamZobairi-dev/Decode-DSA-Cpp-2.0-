a380c59b9fccd10617243ee5fe7dc5281c42cb63
Ahzam <ahzamzobairi402@gmail.com>
2024-02-21 20:32:59 +0530

    Assignment | Week3 | Pattern Printing 2 | Q12
    -- Approach2 Square with hollow diamond cut out

---
+++ Assignment/Wk3/Pattern_Printing_2/11.PP2_approach2.cpp
@@ -0,0 +1,72 @@
+/*      Square with hollow diamond cut out
+
+.          n=5                       n = 4              n = 3         n = 2      n=1
+
+1    * * * * * * * * *            * * * * * * *       * * * * *       * * *       *
+2    * * * * - * * * *            * * * - * * *       * * - * *       * - *
+3    * * * - - - * * *            * * - - - * *       * - - - *       * * *
+4    * * - - - - - * *            * - - - - - *       * * - * * 
+5    * - - - - - - - *            * * - - - * *       * * * * *
+6    * * - - - - - * *            * * * - * * *
+7    * * * - - - * * *            * * * * * * *
+8    * * * * - * * * * 
+9    * * * * * * * * *
+    
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows : ";
+    cin >> n;
+
+    for(int i=1; i<=2*n-1; i++)     cout << "*";
+    cout << endl;
+    int nsp=1;
+    for(int i=1; i<=n-2; i++)
+    {
+        for(int j=n-i; j>=1; j--)
+        {
+            cout << "*";
+        }
+
+        for(int k=1; k<=nsp; k++)
+        {
+            cout << " ";
+        }
+        nsp+=2;
+        for(int l=n-i; l>=1; l--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for(int i=1; i<=n-1; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+
+        for(int k=1; k<=nsp; k++)
+        {
+            cout <<" ";
+        }
+        nsp-=2;
+
+        for(int j=1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+
+        cout << endl;
+    }
+
+    for(int i=1; i<=2*n-1; i++)     cout << "*";
+
+    return 0;
+}
