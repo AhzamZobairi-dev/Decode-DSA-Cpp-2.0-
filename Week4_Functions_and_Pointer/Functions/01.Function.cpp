2c7004494587b02d519a09002d3aa7ec1b372fcc
Ahzam <ahzamzobairi402@gmail.com>
2024-02-22 19:53:02 +0530

    Week4 | Lec 9 | Functions and Pointers
    -- Basic about functions in C++

---
+++ Week4_Functions_and_Pointer/Functions/01.Function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void greeting()
+{
+    cout << "Good Morning"<< endl;
+    cout << "Have a nice day" << endl;
+}
+
+int main()
+{
+    greeting();
+    greeting();
+    greeting();
+
+    return 0;
+}
