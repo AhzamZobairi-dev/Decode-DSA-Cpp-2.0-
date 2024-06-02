67a6a47bfe9d2df6d512fdd9cba0336750171f6c
Ahzam <ahzamzobairi402@gmail.com>
2024-06-03 02:53:31 +0530

    Week 5 | Lecture 14 | Array 3
    Description:
      -- LC 31
      -- Next Permutaion

---
+++ Week5_Array/Array_3/06.Find_next_array_permutation.cpp
@@ -0,0 +1,72 @@
+/*                    Leect Code -- 31. Next Permutation            -- Medium
+
+    https://leetcode.com/problems/next-permutation/description/
+*/          
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverse(vector<int>& nums, int i, int j)
+{
+    while(i<j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+void nextPermutation(vector<int>& v)
+{
+    int idx = -1;
+    int n = v.size();
+    for(int i = n-2; i >= 0; i--)
+    {
+        if(v[i]<v[i+1])
+        {
+            idx = i;
+            break;
+        }
+    }
+
+    if(idx == -1 )
+    {
+        reverse(v, 0, n-1);
+        return;
+    }
+
+    reverse(v, idx+1, n-1);
+
+    int j =-1;
+    for(int i = idx+1; i < n; i++)
+    {
+        if(v[idx]<v[i])
+            j = i;
+    }
+
+    swap(v[idx], v[j]);
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements : ";
+    cin >> n;
+
+    vector<int> seq(n);
+    cout << "Enter the Squence whose next permutation you want : ";
+
+    for(int i = 0; i < n; i++)
+        cin >> seq[i];
+
+    nextPermutation(seq);
+
+    for(int i = 0; i < n; i++)
+        cout << seq[i] << " ";
+
+    return 0;
+}
