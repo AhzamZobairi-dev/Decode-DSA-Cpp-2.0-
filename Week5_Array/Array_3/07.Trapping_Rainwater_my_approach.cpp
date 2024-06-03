e90eeb1da6058fe95584c8b65f6246e0ce2ed3cf
Ahzam <ahzamzobairi402@gmail.com>
2024-06-04 00:41:47 +0530

    Week 5 | Lecture 14 | Array 3
    Description:
      -- LC 42
      -- How much water is trapped in an array while consider array as a
      histogram
    	-- Approch 1 is by findind previous and next highest element and
    	comaring it with actual array
    
    	-- Approach2 is Tabnine generated solution

--- Week5_Array/Array_3/07.Trapping_Rainwater_my_approach.cpp
+++ Week5_Array/Array_3/07.Trapping_Rainwater_my_approach.cpp
@@ -1,5 +1,20 @@
 /*          LeetCode -- 42. Trapping RainWater              HARD
 
+Here we have made 2 different arrays apart from the main array 
+height[ ] = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+1. prev[ ] -- it contains previous greatest element of the array 
+    prev[ ] -- [-1,0,1,1,2,2,2,2,3,3,3,3]
+
+2. next[ ] -- it contains the next greatest element of the array
+    next[ ]  -- [3,3,3,3,3,3,3,2,2,2,1,-1]
+
+Instead of creating an another array We can compare next[ ] and prev[ ] and store it in next [ ] 
+
+ next[ ] --  min(prev[i], next[i])
+   next[ ] -- [-1,0,1,1,2,2,2,2,2,2,1,-1]
+
+now when next[i] > height[i]. then, we can next[i] - height[i] and this the trapped water.
 
 */
 
@@ -32,11 +47,13 @@ int trap(vector<int> &height)
             max = height[i];
     }
 
+/*   We can merge the next two for-loop together
+    -- find minimum and trapped water in single statement
+*/
     for(int i=0; i<n; i++)
     {
         next[i] = min(next[i],prev[i]);
     }
-
 
     int water =0;
     for(int i=0; i<n; i++)
